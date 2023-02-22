d093dd778425248544272f8abcd9ba412fd8eaa8
Adam Bubenicek <adam@adambubenicek.com>
2023-02-22 12:53:12 +0100

    Add mouse-follow animation

--- client.js
+++ client.js
@@ -75,16 +75,58 @@ const scenes = Array.from(
   .reverse();
 
 let currentScene = getCurrentScene();
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+let mouseX = windowWidth / 2;
+let mouseY = windowHeight / 2;
+let followMouse = false;
+/** @type {NodeJS.Timeout} */
+let followMouseTimeout;
+let offsetX = mouseX / windowWidth - 0.5;
+let offsetY = mouseY / windowHeight - 0.5;
+let rigidity = 0;
+
+window.addEventListener("resize", () => {
+  windowWidth = window.innerWidth;
+  windowHeight = window.innerHeight;
+});
+
+window.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+
+  followMouse = true;
+  clearTimeout(followMouseTimeout);
+  followMouseTimeout = setTimeout(() => {
+    followMouse = false;
+    rigidity = 0
+  }, 15000);
+});
 
 /**
  * @param {DOMHighResTimeStamp} time
  */
-function animateAlways(time) {
+function animate(time) {
   const { parallaxAmount } = currentScene;
-  const orbitSpeed = (1 / (Math.PI * parallaxAmount)) * 0.02;
 
-  const newOffsetX = Math.sin(time * orbitSpeed) * 0.5;
-  const newOffsetY = Math.cos(time * orbitSpeed) * 0.5;
+  let newOffsetX = 0;
+  let newOffsetY = 0;
+
+  if (followMouse) {
+    newOffsetX = mouseX / windowWidth - 0.5;
+    newOffsetY = mouseY / windowHeight - 0.5;
+    rigidity = 0.5;
+  } else {
+    const orbitSpeed = (1 / (Math.PI * parallaxAmount)) * 0.02;
+    newOffsetX = Math.sin(time * orbitSpeed) * 0.5;
+    newOffsetY = Math.cos(time * orbitSpeed) * 0.5;
+    if (rigidity < 1) {
+      rigidity += 0.001;
+    }
+  }
+
+  newOffsetX = offsetX + (newOffsetX - offsetX) * rigidity;
+  newOffsetY = offsetY + (newOffsetY - offsetY) * rigidity;
 
   for (let sceneObject of currentScene.objects) {
     const x = newOffsetX * sceneObject.z * parallaxAmount;
@@ -93,7 +135,10 @@ function animateAlways(time) {
     sceneObject.element.style.transform = `translate3d(${x}px, ${y}px, 1px)`;
   }
 
-  requestAnimationFrame(animateAlways);
+  offsetX = newOffsetX;
+  offsetY = newOffsetY;
+
+  requestAnimationFrame(animate);
 }
 
-requestAnimationFrame(animateAlways);
+requestAnimationFrame(animate);
