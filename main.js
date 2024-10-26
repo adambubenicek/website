245c6db15ff67f254c763c67af180b6a6060c316
Adam Bubenicek <adam@adambubenicek.com>
2024-10-26 20:46:04 +0200

    add rudimentary drawing

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-const gridWidth = 48
-const gridHeight = 32
+const gridWidth = 8
+const gridHeight = 8
+const gridSize = 60
 
 
+// Single dimensional array representing the entire grid.
+// Each item is a single byte.
+//
+// 00000000
+// | | | |- left 
+// | | |- bottom 
+// | |- top 
+// |- right 
+//
+// 00 - empty
+// 01 - to this direction
+// 10 - from this direction
 const grid = new Uint8Array(gridWidth * gridHeight)
 
-const originBoxX = 12
-const originBoxY = 8
+const originBoxX = 2
+const originBoxY = 2
 const originBoxWidth = 4
-const originBoxHeight = 3
+const originBoxHeight = 4
 
 const direction = {
   up: 6,
@@ -47,17 +60,66 @@ grow(originBoxX + originBoxWidth, originBoxY + originBoxHeight, direction.left, 
 grow(originBoxX, originBoxY + originBoxHeight, direction.right, values.from)
 grow(originBoxX, originBoxY + originBoxHeight, direction.up, values.to)
 
-for (let i = 1; i < originBoxWidth - 1; i++) {
-  grow(originBoxX, originBoxY, direction.right, values.from)
-  grow(originBoxX, originBoxY, direction.left, values.to)
-  grow(originBoxX, originBoxY + originBoxHeight, direction.right, values.to)
-  grow(originBoxX, originBoxY + originBoxHeight, direction.left, values.from)
+for (let i = 1; i < originBoxWidth; i++) {
+  grow(originBoxX + i, originBoxY, direction.right, values.from)
+  grow(originBoxX + i, originBoxY, direction.left, values.to)
+  grow(originBoxX + i, originBoxY + originBoxHeight, direction.right, values.to)
+  grow(originBoxX + i, originBoxY + originBoxHeight, direction.left, values.from)
 }
 
-for (let i = 1; i < originBoxHeight - 1; i++) {
-  grow(originBoxX, originBoxY, direction.down, values.from)
-  grow(originBoxX, originBoxY, direction.up, values.to)
-  grow(originBoxX + originBoxWidth, originBoxY + originBoxHeight, direction.down, values.to)
-  grow(originBoxX + originBoxWidth, originBoxY + originBoxHeight, direction.up, values.from)
+for (let i = 1; i < originBoxHeight; i++) {
+  grow(originBoxX, originBoxY + i, direction.down, values.from)
+  grow(originBoxX, originBoxY + i, direction.up, values.to)
+  grow(originBoxX + originBoxWidth, originBoxY + i, direction.down, values.to)
+  grow(originBoxX + originBoxWidth, originBoxY + i, direction.up, values.from)
 }
 
+for (let loc in grid) {
+  const y = Math.floor(loc / gridWidth)
+  const x = loc - y * gridWidth;
+  const val = grid[loc]
+
+  if ((val >> 6) > 0) {
+    const el = document.createElement("div")
+    el.style.position = "absolute"
+    el.style.top = `${(y * gridSize) - gridSize / 2}px`
+    el.style.left = `${(x * gridSize)}px`
+    el.style.width = `2px`
+    el.style.height = `${gridSize / 2}px`
+    el.style.background = 'red'
+    document.body.appendChild(el)
+  }
+
+  if ((val >> 4 & 3) > 0) {
+    const el = document.createElement("div")
+    el.style.position = "absolute"
+    el.style.top = `${(y * gridSize)}px`
+    el.style.left = `${(x * gridSize)}px`
+    el.style.height = `2px`
+    el.style.width = `${gridSize / 2}px`
+    el.style.background = 'red'
+    document.body.appendChild(el)
+  }
+
+  if ((val >> 2 & 3) > 0) {
+    const el = document.createElement("div")
+    el.style.position = "absolute"
+    el.style.top = `${(y * gridSize)}px`
+    el.style.left = `${(x * gridSize)}px`
+    el.style.width = `2px`
+    el.style.height = `${gridSize / 2}px`
+    el.style.background = 'red'
+    document.body.appendChild(el)
+  }
+
+  if ((val & 3) > 0) {
+    const el = document.createElement("div")
+    el.style.position = "absolute"
+    el.style.top = `${(y * gridSize)}px`
+    el.style.left = `${(x * gridSize) - gridSize / 2}px`
+    el.style.height = `2px`
+    el.style.width = `${gridSize / 2}px`
+    el.style.background = 'red'
+    document.body.appendChild(el)
+  }
+}
