5eca940a3ea3d03d2dc9f4dbb9838846e75e94f1
Adam Bubenicek <adam@adambubenicek.com>
2024-10-30 08:58:06 +0100

    create library

--- cube.js
+++ cube.js
@@ -1,34 +1,8 @@
 import { mat4, quat, vec3 } from "gl-matrix"
+import { createShader, createProgram } from './lib'
 
 
 export function init(gl) {
-  function createShader(gl, type, source) {
-    const shader = gl.createShader(type)
-    gl.shaderSource(shader, source)
-    gl.compileShader(shader)
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      throw gl.getShaderInfoLog(shader);
-    }
-
-    return shader
-  }
-
-  function createProgram(gl, vertexShader, fragmentShader) {
-    const program = gl.createProgram();
-
-    gl.attachShader(program, vertexShader)
-    gl.attachShader(program, fragmentShader)
-
-    gl.linkProgram(program)
-
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-      throw gl.getProgramInfoLog(program)
-    }
-
-    return program
-  }
-
   const vertexShader = createShader(gl, gl.VERTEX_SHADER, `#version 300 es
 precision highp float;
 
